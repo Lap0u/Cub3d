939cb145b12a97c9db576d116d118d16b2ebc535
cbeaurai <cbeaurai@student.42.fr>
2022-01-07 23:27:25 +0100

    ajout maintien touche

--- raycasting/1.c
+++ raycasting/1.c
@@ -14,7 +14,7 @@ int	main(void)
 	t_data	img;
 
 	init_app(&my_app, "raycasting", 1024, 512);
-	mlx_key_hook(my_app.win, &player_input, &my_app);
+	mlx_hook(my_app.win, 2, 1L << 0, &player_input, &my_app);
 	// mlx_put_image_to_window(my_app.mlx, my_app.win, my_app.brouillon.img, 0, 0);
 	// mlx_put_image_to_window(my_app.mlx, my_app.win, my_app.sp.img, 300, 300);
 	mlx_hook(my_app.win, 17, 0, destroy_game_data, &my_app);
