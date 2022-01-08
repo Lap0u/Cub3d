0c7a458250c9d5edfcfbf8a5868cad595354f8e2
cbeaurai <cbeaurai@student.42.fr>
2022-01-08 23:18:08 +0100

    blocage aux murs fonctionnent pour up et down seulement

--- raycasting/actions.c
+++ raycasting/actions.c
@@ -19,6 +19,33 @@ int	player_input(int key, void *data)
 int	player_input_body(int key, t_app *app)
 {
 	extern int map[];
+	extern int map_x;
+	extern int map_y;
+	int xo, yo, posx, posx_add_xo, posx_sub_xo, posy, posy_add_yo, posy_sub_yo;
+	int posx_add_yo, posx_sub_yo, posy_add_xo, posy_sub_xo;
+
+	if (app->sp.game_state.delta_x < 0)
+		xo = -20; //modifier ces valeurs pour choisir l'ecart min avec le mur 
+	else
+		xo = 20;
+	if (app->sp.game_state.delta_y < 0)
+		yo = -20;
+	else
+		yo = 20;
+	//
+	posx = app->sp.game_state.player_x / 64.0;
+	posx_add_xo = (app->sp.game_state.player_x + xo) / 64.0;
+	posx_sub_xo = (app->sp.game_state.player_x - xo) / 64.0;
+	//
+	posy = app->sp.game_state.player_y / 64.0;
+	posy_add_yo = (app->sp.game_state.player_y + yo) / 64.0;
+	posy_sub_yo = (app->sp.game_state.player_y - yo) / 64.0;
+	//
+	posx_add_yo = (app->sp.game_state.player_x + yo) / 64.0;
+	posx_sub_yo = (app->sp.game_state.player_x - yo) / 64.0;
+	//
+	posy_add_xo = (app->sp.game_state.player_y + xo) / 64.0;
+	posy_sub_xo = (app->sp.game_state.player_y - xo) / 64.0;
 	printf("key: %d, pa = %f\n", key, app->sp.game_state.pa);
 	if (key == FL_LEFT || key == FL_RIGHT
 		||key == LEFT || key == RIGHT || key == DOWN || key == UP)
@@ -47,26 +74,31 @@ int	player_input_body(int key, t_app *app)
 			app->sp.game_state.delta_x = cosf(app->sp.game_state.pa) * 5;
 			app->sp.game_state.delta_y = sinf(app->sp.game_state.pa) * 5;
 		}
-		else if (key == LEFT && map[(int)(((app->sp.game_state.player_y - (app->sp.game_state.delta_x)) / 64) * 8 + (app->sp.game_state.player_x + (app->sp.game_state.delta_y)) / 64)] != 1)
+		else if (key == LEFT)
 		{
-			printf("%d : case_bloc\n", (int)((app->sp.game_state.player_y - (app->sp.game_state.delta_x)) / 64 * 8 + (app->sp.game_state.player_x + (app->sp.game_state.delta_y)) / 64));
-			app->sp.game_state.player_y -= (app->sp.game_state.delta_x) / 1;
-			app->sp.game_state.player_x += (app->sp.game_state.delta_y) / 1;
+			if (map[posx_add_yo * map_x + posx] != 1)
+				app->sp.game_state.player_x += (app->sp.game_state.delta_y) / 1;
+			if (map[posy * map_x + posy_sub_xo] != 1)
+				app->sp.game_state.player_y -= (app->sp.game_state.delta_x) / 1;
 		}
 		else if (key == RIGHT)
 		{
-			app->sp.game_state.player_y += (app->sp.game_state.delta_x) / 1;
 			app->sp.game_state.player_x -= (app->sp.game_state.delta_y) / 1;
+			app->sp.game_state.player_y += (app->sp.game_state.delta_x) / 1;
 		}
 		else if (key == DOWN)
 		{
-			app->sp.game_state.player_x -= (app->sp.game_state.delta_x) / 1;
-			app->sp.game_state.player_y -= (app->sp.game_state.delta_y) / 1;
+			if (map[posy * map_x + posx_sub_xo] != 1)
+				app->sp.game_state.player_x -= (app->sp.game_state.delta_x) / 1;
+			if (map[posy_sub_yo * map_x + posx] != 1)	
+				app->sp.game_state.player_y -= (app->sp.game_state.delta_y) / 1;
 		}
 		else if (key == UP)
 		{
-			app->sp.game_state.player_x += (app->sp.game_state.delta_x) / 1;
-			app->sp.game_state.player_y += (app->sp.game_state.delta_y) / 1;
+			if (map[posy * map_x + posx_add_xo] != 1)
+				app->sp.game_state.player_x += (app->sp.game_state.delta_x) / 1;
+			if (map[posy_add_yo * map_x + posx] != 1)
+				app->sp.game_state.player_y += (app->sp.game_state.delta_y) / 1;
 		}
 		// if (check_pos_player(app))
 		// {
