4e09b325a7afb15519487b97592d6d0d8fef6bda
Clement BEAURAIN <cbeaurai@e2r11p6.clusters.42paris.fr>
2022-01-17 13:15:16 +0100

    dessin mini map ++

--- raycasting/map.c
+++ raycasting/map.c
@@ -30,6 +30,25 @@ void	drow_element(t_app *app, int x, int y, int color)
 	}
 }
 
+void	draw_grid(t_app *app, int size)
+{
+	int x;
+	int y;
+
+	x = 1;
+	while (x < 192)
+	{
+		y = 1;
+		while (y < 192)
+		{
+			if (y % (192 / size) == 0 || x % (192 / size) == 0)
+				my_mlx_pixel_put(&(app->img), x, y, 0x0000FF);
+			y++;
+		}
+		x++;
+	}
+}
+
 void	add_empty_tile(t_app *app, int x, int y, int size)
 {
 	int i;
@@ -41,9 +60,71 @@ void	add_empty_tile(t_app *app, int x, int y, int size)
 		j = 0;
 		while (j < 192 / size)
 		{
-			my_mlx_pixel_put(&(app->img), x * (192 / size) + i, y * (192 / size) + j, 0x444444);
+			my_mlx_pixel_put(&(app->img), x * (192 / size) + i, y * (192 / size) + j, 0x333333);
 			j++;
 		}
+		i++;
+	}
+}
+
+void	add_wall_tile(t_app *app, int x, int y, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < 192 / size)
+	{
+		j = 0;
+		while (j < 192 / size)
+		{
+			my_mlx_pixel_put(&(app->img), x * (192 / size) + i, y * (192 / size) + j, 0x999999);
+			j++;
+		}
+		i++;
+	}
+}
+
+void	add_full_tile(t_app *app, int x, int y, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < 192 / size)
+	{
+		j = 0;
+		while (j < 192 / size)
+		{
+			my_mlx_pixel_put(&(app->img), x * (192 / size) + i, y * (192 / size) + j, 0xEEEEEE);
+			j++;
+		}
+		i++;
+	}
+}
+
+void	draw_mini_player(t_app *app)//a deplacer un peu en fonction du decalage avec le mur
+{
+	int	x;
+	int	y;
+	int	i;
+
+	x = 0;
+	i = 0;
+	while (x < 4)
+	{
+		y = 0;
+		while (y < 4)
+		{
+			my_mlx_pixel_put(&(app->img), 192 / 2  - 2 + x, 192 / 2 - 2 + y, 0xFF0000);
+			y++;
+		}
+		x++;
+	}
+	while (i < 15)
+	{
+		my_mlx_pixel_put(&(app->img), (192 / 2 + (i * cos(app->ray.game_state.pa)/2)),
+		(192 / 2 + (i * sin(app->ray.game_state.pa)/2)), 0xFF0000);
 		i++;
 	}
 }
@@ -69,15 +150,19 @@ void	draw_around_player(t_app *app, float p_x, float p_y, int size)
 		while (j < size)
 		{
 			if (s_x < 0 || s_y < 0 || s_x > map_x || s_y > map_y)
-				add_empty_tile(app, j, i, size);
-			// else
-			// 	add_full_tile(app, s_x, s_y, size);
+				add_empty_tile(app, j, i, size); //check si la map est un espace
+			else//check si la map est un mur
+				add_wall_tile(app, j, i, size);
+			// else//si la map est un sol (0)
+			// 	add_full_tile(app, j, i, size);
 			j++;
 			s_x++;
 		}
 		i++;
 		s_y++;
 	}
+	draw_mini_player(app);
+	draw_grid(app, size);
 }
 
 void draw_map(t_app *app)
